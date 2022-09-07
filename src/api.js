7515ca283e432aff216dd9d5dc0c75a218a18afd
Chaitanya <chaitu.vsh@gmail.com>
2022-09-07 22:56:33 +0530

    Enable cors for the APIs

--- src/api.js
+++ src/api.js
@@ -1,8 +1,13 @@
 const express = require("express");
+
+const cors = require("cors");
 const serverless = require("serverless-http");
 const routes = require("./routes")
 
 const app = express();
+
+// Enable CORS for the frontend domain to call the APIs hosted on this server
+app.use(cors())
 
 app.use(`/.netlify/functions/api`, routes);
 
